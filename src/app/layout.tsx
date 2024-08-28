44201f62da38488599a4b1164c9c11a0e748a038
maxamed <maxamedadan169@gmail.com>
2024-08-28 23:27:46 +0300

    building the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <div className="container flex justify-between py-4 px-6 mx-auto">
+            <Link href={"/"} className="font-bold">Job Board</Link>
+
+            <nav className="flex gap-4 *:py-2 *:px-4 *:rounded-md">
+              <Link className="bg-gray-200" href={"/login"}>Login</Link>
+              <Link className="bg-blue-600 text-white" href={"/new-listing"}>Post a job</Link>
+            </nav>
+          </div>
+        </header>
+
+        {children}
+      </body>
     </html>
   );
 }
