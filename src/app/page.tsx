ddc71eb07ab524f7b366ce354f9de9b4b807f9be
maxamed <maxamedadan169@gmail.com>
2024-08-30 00:37:29 +0300

    fixed the nav response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Jobs from "./components/Jobs";
-import About from "./about/page";
-import Contact from "./contact/page";
+// import About from "./about/page";
+// import Contact from "./contact/page";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Jobs />
-      <About />
-      <Contact />
+      {/* <About />
+      <Contact /> */}
     </>
   );
 }
