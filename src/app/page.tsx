c9c27cbecd9b54fdbad14a18af0d0dde48919f75
maxamed <maxamedadan169@gmail.com>
2024-08-30 01:05:54 +0300

    creating authantication with workOs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Jobs from "./components/Jobs";
-// import About from "./about/page";
-// import Contact from "./contact/page";
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from "@workos-inc/authkit-nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const { user } = await getUser();
+
+  // Get the URL to redirect the user to AuthKit to sign in
+  const signInUrl = await getSignInUrl();
+
+  // Get the URL to redirect the user to AuthKit to sign up
+  const signUpUrl = await getSignUpUrl();
   return (
     <>
       <Hero />
---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
+
+export default authkitMiddleware();
+
+// Match against pages that require authentication
+// Leave this out if you want authentication on every page in your application
+export const config = {
+  matcher: ["/"],
+};
