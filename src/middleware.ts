4bef799b37e41f371617e3dc11496ebd37144641
maxamed <maxamedadan169@gmail.com>
2024-08-30 01:21:59 +0300

    fixed some bugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
-import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
+import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
 
 export default authkitMiddleware();
 
 // Match against pages that require authentication
 // Leave this out if you want authentication on every page in your application
-export const config = {
-  matcher: ["/"],
-};
+export const config = { matcher: ['/'] };
