8081d60b0fab68487acceb8a888cc77338984a52
maxamed <maxamedadan169@gmail.com>
2024-08-29 14:54:37 +0300

    building the job section

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -1,19 +1,21 @@
 export default function JobRow() {
   return (
     <>
-      <div className="bg-white p-4 rounded-lg shadow-sm flex gap-4">
-        <div className="content-center">
-          <img
-            className="size-12"
-            src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png"
-            alt=""
-          />
-        </div>
-        <div>
-          <div className="text-gray-500 text-sm">Spotify</div>
-          <div className="font-bold text-lg mb-1">Product designer</div>
-          <div text-gray-500 text-sm>
-            Remote &middot; New York US &middot; Full-time
+      <div className="bg-white p-4 rounded-lg shadow-sm md:flex">
+        <div className=" flex gap-4">
+          <div className="content-center">
+            <img
+              className="size-12"
+              src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png"
+              alt=""
+            />
+          </div>
+          <div className="grow">
+            <div className="text-gray-500 text-sm">Spotify</div>
+            <div className="font-bold text-lg mb-1">Product designer</div>
+            <div text-gray-500 text-sm>
+              Remote &middot; New York US &middot; Full-time
+            </div>
           </div>
         </div>
         <div className="content-end text-gray-400 text-sm">2 weeks ago</div>
