5f35a3b387c24425f0f23a97e858918a23ae4dac
moh123adan <maxamedadan169@gmail.com>
2024-10-15 16:43:19 +0300

    Deleted: some fiels

--- src/app/actions/authActions.ts
+++
@@ -1,9 +0,0 @@
-// /app/actions/authActions.js (or .ts if using TypeScript)
-"use server"; // This marks the file as a server action
-
-import { signOut } from "@workos-inc/authkit-nextjs";
-
-// Define the logout action
-export async function logoutAction() {
-    await signOut();
-}
