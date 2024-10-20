e3e058b68d638ea81ea85cd60df3da06a05ec0f2
moh123adan <maxamedadan169@gmail.com>
2024-10-20 21:02:52 +0300

    created: some logic of thr workos

--- src/app/new-listing/[orgId]/page.tsx
+++ src/app/new-listing/[orgId]/page.tsx
@@ -1,8 +1,31 @@
-export default function NewListingForOrgPage() {
-  return <form action="" className="container mt-6">
-    
+import { getUser } from "@workos-inc/authkit-nextjs";
+import { WorkOS } from "@workos-inc/node";
 
-    
-    
-    new job form here</form>;
+type PageProps = {
+  params: {
+    orgId: string;
+  };
+};
+
+export default async function NewListingForOrgPage(props: PageProps) {
+  const { user } = await getUser();
+
+  const workos = new WorkOS(process.env.WORKOS_API_KEY);
+
+  if (!user) return <div>Please log in</div>;
+  const orgId = props.params.orgId;
+
+  const oms = await workos.userManagement.listOrganizationMemberships({ userId: user.id });
+
+  const om = oms.data.find(om => om.organizationId === orgId);
+
+  return JSON.stringify(om);
+
+
+  return (
+    <form action="" className="container mt-6">
+      {JSON.stringify(props)}
+      new job form here
+    </form>
+  );
 }
