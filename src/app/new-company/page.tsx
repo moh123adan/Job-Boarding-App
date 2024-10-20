3d8e52cd30754dd171d5d0bb90e8ea19f9278c83
moh123adan <maxamedadan169@gmail.com>
2024-10-20 16:17:14 +0300

    created the new company component or page

--- src/app/new-company/page.tsx
+++ src/app/new-company/page.tsx
@@ -1,7 +1,40 @@
-export default function NewCompanyPage () {
-    return (
-        <div>
-            
-        </div>
-    )
+import { getUser } from "@workos-inc/authkit-nextjs";
+import createCompany from "../actions/workosActions";
+
+export default async function NewCompanyPage() {
+  const { user } = await getUser();
+
+  async function handleNewCompanyFormSubmit(data: FormData) {
+    "use server";
+    if (user) {
+      await createCompany(data.get("newCompanyName") as string, user.id);
+    }
+  }
+
+  if (!user) {
+    ("Loggin to use this page");
+  }
+
+  return (
+    <div className="container">
+      <h2 className="text-lg">Create new company</h2>
+      <p className="text-gray-500 text-sm mb-2">
+        To create a job listing, you first need to register a company.
+      </p>
+      <form action={handleNewCompanyFormSubmit} className="flex gap-2">
+        <input
+          name="newCompanyName"
+          className="p-2 border border-gray-400 rounded-md"
+          type="text"
+          placeholder="Company name"
+        />
+        <button
+          type="submit"
+          className="flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md"
+        >
+          Create company
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -10,12 +10,7 @@ export default async function NewListingPage() {
   const workos = new WorkOS(process.env.WORKOS_API_KEY);
   const { user } = await getUser();
 
-  async function handleNewCompanyFormSubmit(data: FormData) {
-    "use server";
-    if (user) {
-      await createCompany(data.get("newCompanyName") as string, user.id);
-    }
-  }
+
   // If the user is not logged in, return a message
   if (!user) {
     return (
@@ -48,24 +43,8 @@ export default async function NewListingPage() {
           No companies found assigned to your user
         </div>
 
-        <h2 className="text-lg">Create new company</h2>
-        <p className="text-gray-500 text-sm mb-2">
-          To create a job listing, you first need to register a company.
-        </p>
-        <form action={handleNewCompanyFormSubmit} className="flex gap-2">
-          <input
-            name="newCompanyName"
-            className="p-2 border border-gray-400 rounded-md"
-            type="text"
-            placeholder="Company name"
-          />
-          <button
-            type="submit"
-            className="flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md"
-          >
-            Create company
-          </button>
-        </form>
+        <Link href=""></Link>
+      
       </div>
     </div>
   );
