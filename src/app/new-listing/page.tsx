f98e027ee728128ff9d95d01e6b450736e3b15a0
moh123adan <maxamedadan169@gmail.com>
2024-10-20 14:35:21 +0300

    added: button and input for the jop post

--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -1,14 +1,21 @@
 import { getUser } from "@workos-inc/authkit-nextjs";
-// import { WorkOS } from "@workos-inc/node";
+import { WorkOS } from "@workos-inc/node";
 // import Link from "next/link";
 // import toast from "react-hot-toast";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default async function NewListingPage() {
-  // const workos = new WorkOS(process.env.WORKOS_API_KEY);
+  const workos = new WorkOS(process.env.WORKOS_API_KEY);
 
   const { user } = await getUser();
+
+  if (user) {
+    const organizationMemberShip =
+      await workos.userManagement.listOrganizationMemberships({
+        userId: user.id,
+      });
+  }
 
   // if (!user) {
   //   return <div>toast.success("you need to be logged in to post job")</div>;
@@ -19,34 +26,46 @@ export default async function NewListingPage() {
   //     userId: user.id,
   //   });
 
-  //   const activeOrganizationMemberShips = oragnizationMemberShips.data.filter(om => om.status === "active");
+  // const activeOrganizationMemberShips = organizationMemberShips.data.filter(om => om.status === "active");
 
-  //   const oragnizationNames:{[key: string]: string} = {};
+  // const organizationNames: { [key: string]: string } = {};
 
-  //   for(activeMembership of activeOrganizationMemberShips) {
-  //     const organization = await oragnization.getOrganization(activeMembership.organizationId);
-
-  //     oragnizationNames[organizationId] = organizationName;
-  //   }
+  // for (let activeMembership of activeOrganizationMemberShips) {
+  //   const organization = await organization.getOrganization(activeMembership.organizationId);
+  //   organizationNames[organizationId] = organization.name;
+  // }
 
   return (
     <div className="container mt-42">
-      {!user && <div>You need to be logged in to post</div>}
-      {user && (
-        <div>
-          <form action="">
-            <div>
-              <h2 className="text-lg">Create new company</h2>
-              <p className="text-gray-500">
-                To Create a job listing your first need to register company
-              </p>
-              <button className="bg-gray-200 px-4 py-2 rounded-md">
-                Create company <FontAwesomeIcon icon={faArrowRight} />
-              </button>
+      <div>
+        {!user && <div>You need to be logged in to post</div>}
+        {user && (
+          <div>
+            <h2 className="text-lg mt-6">Your companies</h2>
+            <p className="text-gray-500 text-sm mb-2">
+              Select a company to create a job add for{" "}
+            </p>
+            <div className="border border-red-200 bg-red-50 p-4 rounded-md">
+              No companies found assigned to your user
             </div>
-          </form>
-        </div>
-      )}
+
+            <h2 className="text-lg">Create new company</h2>
+            <p className="text-gray-500 text-sm mb-2">
+              To create a job listing, you first need to register a company.
+            </p>
+            <form action="" className="flex gap-2">
+              <input
+                className="p-2 border border-gray-400 rounded-md mb-4"
+                type="text"
+                placeholder="Company name"
+              />
+              <button className="flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md">
+                Create company
+              </button>
+            </form>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
