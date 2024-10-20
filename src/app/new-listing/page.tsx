ce540a230edba3eb8666f9ef2a8af998a443716c
moh123adan <maxamedadan169@gmail.com>
2024-10-20 15:23:25 +0300

    fixed: error creating company

--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -6,6 +6,13 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default async function NewListingPage() {
+  async function createCompany(data: FormData) {
+    "use server";
+    await workos.organizations.createOrganization({
+      name: data.get("newCompanyName") as string,
+    });
+  }
+
   const workos = new WorkOS(process.env.WORKOS_API_KEY);
 
   const { user } = await getUser();
@@ -29,11 +36,11 @@ export default async function NewListingPage() {
     <div className="container mt-42">
       <div>
         <pre>
-              {/* Display organization memberships */}
-        {organizationMemberships && JSON.stringify(organizationMemberships, null, 2)}
-
+          {/* Display organization memberships */}
+          {organizationMemberships &&
+            JSON.stringify(organizationMemberships, null, 2)}
         </pre>
-    
+
         <h2 className="text-lg mt-6">Your companies</h2>
         <p className="text-gray-500 text-sm mb-2">
           Select a company to create a job ad for{" "}
@@ -46,18 +53,17 @@ export default async function NewListingPage() {
         <p className="text-gray-500 text-sm mb-2">
           To create a job listing, you first need to register a company.
         </p>
-        <form action={() => {
-          "use server";
-          workos.organizations.createOrganization
-
-        }} 
-        className="flex gap-2">
+        <form action={createCompany} className="flex gap-2">
           <input
+            name="newCompanyName"
             className="p-2 border border-gray-400 rounded-md"
             type="text"
             placeholder="Company name"
           />
-          <button type="submit" className="flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md">
+          <button
+            type="submit"
+            className="flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md"
+          >
             Create company
           </button>
         </form>
