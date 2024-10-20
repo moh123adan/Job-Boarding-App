3602e0a5339a5348f1101365bc0ed953f8ea798d
moh123adan <maxamedadan169@gmail.com>
2024-10-20 16:24:11 +0300

    created: route for the new-company

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,4 +4,4 @@ export default authkitMiddleware();
 
 // Match against pages that require authentication
 // Leave this out if you want authentication on every page in your application
-export const config = { matcher: ['/', '/new-listing',] };
+export const config = { matcher: ['/', '/new-listing', '/new-company'] };
