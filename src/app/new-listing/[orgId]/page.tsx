ee02ed71f8207c6cea33c08d8bc182e71cbec2aa
moh123adan <maxamedadan169@gmail.com>
2024-10-22 16:37:01 +0300

    changed: some code in the orgId page

--- src/app/new-listing/[orgId]/page.tsx
+++ src/app/new-listing/[orgId]/page.tsx
@@ -1,6 +1,5 @@
 import { getUser } from "@workos-inc/authkit-nextjs";
 import { WorkOS } from "@workos-inc/node";
-
 type PageProps = {
   params: {
     orgId: string;
@@ -9,18 +8,20 @@ type PageProps = {
 
 export default async function NewListingForOrgPage(props: PageProps) {
   const { user } = await getUser();
-
   const workos = new WorkOS(process.env.WORKOS_API_KEY);
-
-  if (!user) return <div>Please log in</div>;
+  if (!user) {
+    return "Please log in";
+  }
   const orgId = props.params.orgId;
+  const oms = await workos.userManagement.listOrganizationMemberships({
+    userId: user.id,
+    organizationId: orgId,
+  });
+  const hasAccess = oms.data.length > 0;
 
-  const oms = await workos.userManagement.listOrganizationMemberships({ userId: user.id });
+  
 
-  const om = oms.data.find(om => om.organizationId === orgId);
-
-  return JSON.stringify(om);
-
+  return JSON.stringify({ hasAccess: hasAccess });
 
   return (
     <form action="" className="container mt-6">
